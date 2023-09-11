d319409ccfb9cb5d5069f8fb5145a48bafe6ec24
mohamed.fazal <mohamed.fazal@wdc.com>
2023-09-11 15:26:57 +0530

    added logging

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from lib_wdckit import WDCKit
 from terminaltables import SingleTable
 import sys
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(levelname)s | %(message)s',
+    datefmt='%H:%M:%S'
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
 
 wdckit = WDCKit()
 
@@ -19,17 +30,17 @@ selected_dut = select_dut()
 number_of_cycles = int(input("Enter the number of cycles: "))
 
 for cycle in range(number_of_cycles):
-    print(f"Cycle: {cycle+1}")
+    logging.info(f"Cycle: {cycle+1}")
     result = True
     for ps in range(1,6):
         for transition in [0,ps]:
-            print(f"Setting Power State: PS{transition}")
+            logging.info(f"Setting Power State: PS{transition}")
             set_status = wdckit.set_power_state(selected_dut, transition)
             if not set_status:
                 raise Exception("Unable to change Power State using SetFeature")
             power_state = wdckit.get_power_state(selected_dut)
             result = True if power_state == transition else False
-            print(f"Current Power State: PS{power_state}\tTest Status: {'Pass' if result == True else 'Fail'}")
+            logging.info(f"Current Power State: PS{power_state}\tTest Status: {'Pass' if result == True else 'Fail'}")
             if result == False:
                 raise Exception("Unable to perform PST; Test FAILED!")
 sys.exit()
